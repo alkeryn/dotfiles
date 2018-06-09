ac3883aa2dd4b7964079238ec6e6b1f6132ca95c
Alkeryn <plbraundev@gmail.com>
2018-06-09 03:03:43 +0200

    preparing for merging with laptop configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,5 +4,5 @@ killall -q polybar &
 while pgrep -x polybar >/dev/null; do sleep 1; done
 while pgrep -x stalonetray >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
-polybar bar -q &
+$mainpc && polybar mainpc -q &
 echo "Bars launched..."
