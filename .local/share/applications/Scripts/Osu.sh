f14dd27439d39823efca66e6b921095e30f4aaf4
Alkeryn <plbraundev@gmail.com>
2019-03-16 10:37:43 +0100

    mimeapps and Osu.sh edited

--- .local/share/applications/Scripts/Osu.sh
+++ .local/share/applications/Scripts/Osu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pkill compton;env WINEPREFIX=/home/alkeryn/App/wine/Osu wine /home/alkeryn/App/wine/Osu/drive_c/Osu/osu\!.exe;compton & #--xrender-sync-fence
+pkill compton;env WINEPREFIX=/home/alkeryn/App/wine/Osu wine /home/alkeryn/App/wine/Osu/drive_c/Osu/osu\!.exe;compton -b #--xrender-sync-fence
