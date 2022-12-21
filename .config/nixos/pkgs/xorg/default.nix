97c131bedd56aaf22b2d42a79b91b7b5c0a6d09f
alkeryn <plbraundev@gmail.com>
2022-12-21 15:28:09 +0100

    nixos xlogin

--- .config/nixos/pkgs/xorg/default.nix
+++ .config/nixos/pkgs/xorg/default.nix
@@ -3,6 +3,7 @@
 let
   overlays = with pkgs; [ ueberzug ];
   qt = with pkgs; [ libsForQt5.qtstyleplugins ];
+  xtty = "7";
 in
 {
   imports = [
@@ -44,5 +45,27 @@ in
     layout = "fr";
     windowManager.bspwm.enable = true;
     displayManager.startx.enable = true;
+  };
+
+  systemd.defaultUnit = "graphical.target";
+  systemd.services.xlogin = {
+    enable = true;
+    description = "X11 for alkeryn";
+    after = [ "graphical.target" "systemd-user-sessions.service" ];
+    wantedBy = [ "graphical.target" ];
+    serviceConfig = {
+      User = "alkeryn";
+      WorkingDirectory = "~";
+      PAMName = "login";
+      Environment = [ "XDG_SESSION_TYPE=x11" ];
+      TTYPath = "/dev/tty${xtty}";
+      StandardInput = "tty";
+      UnsetEnvironment = "TERM";
+      UtmpIdentifier = "tty${xtty}";
+      UtmpMode = "user";
+      StandardOutput = "tty";
+      ExecStartPre = "${pkgs.kbd}/bin/chvt ${xtty}";
+      ExecStart = "${pkgs.xorg.xinit}/bin/startx";
+    };
   };
 }
